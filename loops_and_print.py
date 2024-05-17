be9fe2e19ed7d7629a6f9fb2db22a0d73f3e5a2f
lulibetelu <162722380+lulibetelu@users.noreply.github.com>
2024-05-17 16:00:49 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,21 @@
 def enumerate_list(list):
-    return list
-
+    new_list = []
+    index_new = 0
+    for element in list:
+        if element != "":
+            new_element = f"{index_new}. {element}"
+            new_list.append(new_element)
+            index_new += 1
+    return new_list
 
 def enumerate_backwards(list):
-    return list
+    new_list = []
+    index_new = 0
+    for element in list:
+        if element != "":
+            new_element = f"{index_new}. {element[::-1]}"
+            new_list.append(new_element)
+            index_new += 1
+    return new_list
+
+    
